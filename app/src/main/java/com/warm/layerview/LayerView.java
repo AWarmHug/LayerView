d54a4837b1b67f88a133a1bd7a6bc7a1bc38a254
Warm-mac <865741452@qq.com>
2017-06-06 11:53:03 +0800

    初步实现。

--- app/src/main/java/com/warm/layerview/LayerView.java
+++ app/src/main/java/com/warm/layerview/LayerView.java
@@ -1,16 +1,20 @@
 package com.warm.layerview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
 /**
@@ -19,68 +23,65 @@ import android.widget.FrameLayout;
  * 简介:
  */
 
-public class LayerView extends View {
+public class LayerView extends View implements ViewTreeObserver.OnGlobalLayoutListener {
     private static final String TAG = "LayerView";
     private ViewGroup parent;
     private View showView;
 
 
     //绘制外部的画笔
-    private Paint mPaint;
+    private Paint bgPaint;
 
-    private RectF contentR=new RectF();
+    //绘制按钮和文字的笔
+    private Paint btPaint;
+
+
+    private Rect cRectF = new Rect();
 
 
     /**
      * 控件宽高
      */
-    protected int viewWidth,viewHeight;
+    protected int viewWidth, viewHeight;
     /**
      * 真实可以绘制的宽高,除去padding
      */
-    protected int mWidth,mHeight;
-
+    protected int mWidth, mHeight;
 
 
     public LayerView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public LayerView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public LayerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context,attrs,defStyleAttr);
+        init(context, attrs, defStyleAttr);
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        mPaint=new Paint();
-//        mPaint.setAlpha(155);
-        mPaint.setColor(Color.argb(122,0,0,0));
+        bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        bgPaint.setAlpha(155);
+        btPaint = new Paint();
+        btPaint.setColor(Color.WHITE);
+        btPaint.setAntiAlias(true);
+        btPaint.setStyle(Paint.Style.STROKE);
 
     }
 
-    public void setView(View view){
-        this.showView=view;
-        this.parent=findSuitableParent(view);
-        Log.d(TAG, "setView: "+showView.getX());
-        Rect rect=new Rect();
-        showView.getGlobalVisibleRect(rect);
-
-        if (parent!=null)
-            parent.addView(this);
-        invalidate();
+    public void setView(View view) {
+        this.showView = view;
+        showView.getViewTreeObserver().addOnGlobalLayoutListener(this);
 
     }
-    
 
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
         setMeasuredDimension(measureWidthSpec(widthMeasureSpec), measureHeightSpec(heightMeasureSpec));
     }
 
@@ -122,27 +123,103 @@ public class LayerView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        viewWidth=w;
-        viewHeight=h;
-        mWidth=viewWidth-getPaddingLeft()-getPaddingRight();
-        mHeight=viewHeight-getPaddingTop()-getPaddingBottom();
+        viewWidth = w;
+        viewHeight = h;
+        mWidth = viewWidth - getPaddingLeft() - getPaddingRight();
+        mHeight = viewHeight - getPaddingTop() - getPaddingBottom();
     }
+
+    private Bitmap bgBitmap;
+    private Canvas mCanvas;
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if (parent!=null){
-            RectF rectF=new RectF();
-            rectF.left = 0;
-            rectF.right = mWidth;
-            rectF.top = 0;
-            rectF.bottom = mHeight;
-            canvas.drawRect(rectF,mPaint);
+        if (parent != null) {
+
+//            drawBack1(canvas);
+
+            drawBack2(canvas);
+            drawButton(canvas);
+
+
+
         }
     }
 
+    /**
+     * 绘制按钮
+     *
+     * @param canvas
+     */
+    private void drawButton(Canvas canvas) {
+        int length = 100;
+        int wide = 200;
+        RectF rectF = new RectF((mWidth - wide) / 2,  (mHeight * (2f / 3)), (mWidth + wide) / 2,(mHeight * (2f / 3)) + length);
 
+        canvas.drawRoundRect(rectF,10,10, btPaint);
+
+        btPaint.setTextSize(40);
+        btPaint.setTextAlign(Paint.Align.CENTER);
+        Paint.FontMetrics fontMetrics = btPaint.getFontMetrics();
+        float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
+        float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
+        int baseLineY = (int) (rectF.centerY() - top/2 - bottom/2);//基线中间点的y轴计算公式
+        canvas.drawText("朕知道了",rectF.centerX(),baseLineY,btPaint);
+    }
+
+    private void drawBack2(Canvas canvas) {
+        //设置背景色
+//            canvas.drawColor();
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+        int layerId = canvas.saveLayer(0, 0, canvasWidth, canvasHeight, null, Canvas.ALL_SAVE_FLAG);
+        //正常绘制黄色的圆形
+        bgPaint.setColor(Color.parseColor("#75000000"));
+        RectF rectF = new RectF();
+        rectF.left = 0;
+        rectF.top = 0;
+        rectF.right = mWidth;
+        rectF.bottom = mHeight;
+        canvas.drawRect(rectF, bgPaint);
+        //使用CLEAR作为PorterDuffXfermode绘制蓝色的矩形
+        bgPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+        bgPaint.setColor(Color.TRANSPARENT);
+
+        canvas.drawRect(cRectF, bgPaint);
+        //最后将画笔去除Xfermode
+        bgPaint.setXfermode(null);
+        canvas.restoreToCount(layerId);
+    }
+
+    private void drawBack1(Canvas canvas) {
+        bgBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(bgBitmap);
+        RectF rectF = new RectF();
+        rectF.left = 0;
+        rectF.top = 0;
+        rectF.right = mWidth;
+        rectF.bottom = mHeight;
+        mCanvas.drawRect(rectF, bgPaint);
+        Paint cPaint = new Paint();
+        cPaint.setColor(Color.TRANSPARENT);
+        cPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+        cPaint.setAntiAlias(true);
+        cRectF.left = 0;
+        cRectF.top = 0;
+        cRectF.right = 200;
+        cRectF.bottom = 200;
+        mCanvas.drawRect(cRectF, cPaint);
+
+        canvas.drawBitmap(bgBitmap, 0, 0, bgPaint);
+    }
+
+    /**
+     * @param view
+     * @return
+     */
     private ViewGroup findSuitableParent(View view) {
         ViewGroup fallback = null;
         do {
@@ -168,6 +245,45 @@ public class LayerView extends View {
         return fallback;
     }
 
+    /**
+     * @return statuBar高度
+     */
+    public int getBarHeight() {
+
+        /**
+         * 获取状态栏高度——方法1
+         * */
+        int statusBarHeight = 0;
+        //获取status_bar_height资源的ID
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return statusBarHeight;
+    }
 
 
+    @Override
+    public void onGlobalLayout() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            showView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        } else {
+            showView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+        }
+        int[] location = new int[2];
+
+        showView.getLocationInWindow(location);
+
+        cRectF.left = location[0];
+        cRectF.top = location[1] - getBarHeight();
+        cRectF.right = cRectF.left + showView.getWidth();
+        cRectF.bottom = cRectF.top + showView.getHeight();
+
+        this.parent = findSuitableParent(showView);
+
+        if (parent != null)
+            parent.addView(this);
+    }
 }
--- app/src/main/java/com/warm/layerview/MainActivity.java
+++ app/src/main/java/com/warm/layerview/MainActivity.java
@@ -1,18 +1,27 @@
 package com.warm.layerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity111";
+    private Toolbar tb;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LayerView layerView=new LayerView(this);
+        tb = (Toolbar) this.findViewById(R.id.tb);
+        tv= (TextView) this.findViewById(R.id.tv);
+        tb.inflateMenu(R.menu.tb_menu);
+        LayerView layerView = new LayerView(this);
         layerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        layerView.setView(findViewById(R.id.tv));
+        layerView.setView(tv);
 
     }
+
 }
